b4cb1f591487f8ddf0a7160436ff34b2ab9f3b29
Brynn <tehbunk@gmail.com>
2025-06-11 10:36:30 -0500

    update: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, Suspense } from "react";
-// import ReallyLargeComponent from "./components/ReallyLargeComponent";
+import React from "react";
+import ReallyLargeComponent from "./components/ReallyLargeComponent";
 
 /* LIVE CODING CHALLENGE INSTRUCTIONS
    1. Implement lazy loading for ReallyLargeComponent 
@@ -26,55 +26,27 @@ export type Country = {
   // TODO: You will add a derived field: populationDensity
 };
 
-// TODO: Lazy load ReallyLargeComponent below using React.lazy + Suspense.
-// This simulates a heavy component. Make sure it doesn't block page load.
-const ReallyLargeComponent = React.lazy(() => import("./components/ReallyLargeComponent"));
-
 export default function App() {
-  const [countries, setCountries] = useState<Country[]>([]);
-  // TODO: Add state for isLoading, error, and denseCountries
+  // TODO: Add state for isLoading, error, and countries
 
-  useEffect(() => {
-    // TODO: Fetch countries from the API above
-    // Set loading and error state accordingly
-    // Store the raw data in `countries`
-
-    // Example API endpoint: https://restcountries.com/v2/all
-
-    // TODO: Add populationDensity = population / area (rounded to 2 decimals)
-
-    // TODO: Store filtered countries in a new state variable (e.g. denseCountries)
-    // Filter for countries where populationDensity > 100
-
-  }, []);
+  // TODO: Fetch countries from the API above
+  // Set loading and error state accordingly
+  // Store the raw data in `countries`
+  // Example API endpoint: https://restcountries.com/v2/all
+  // TODO: Add populationDensity = population / area (rounded to 2 decimals)
+  // TODO: Store filtered countries in a new state variable (e.g. denseCountries)
+  // Filter for countries where populationDensity > 100
 
   return (
     <div className="app-container">
       <h1>Countries Population Density Explorer</h1>
 
-      {/* Fixed: Lazy-loaded component causing slow page */}
       <div className="heavy-component-container">
-          {/* TODO: Wrap with Suspense and add fallback */}
-          <ReallyLargeComponent />
+        {/* TODO: Replace with Lazy loading and show a "Loading..." fallback */}
+        <ReallyLargeComponent />
       </div>
 
-      {/* Handle loading & error states */}
-      <div className="error" >
-        <div className="loading" />
-
-        {/* TODO: Render a list of dense countries
-         Include: name, capital, population, populationDensity, and flag */}
-        <div className="countries-container">
-          {/* Your rendering implementation will go here */}
-        </div>
-
-        {/* 
-      BONUS QUESTION:
-      If this data came from your own API:
-      - How would you implement a `createCountry` or `updateCountry` feature?
-      - Would you use optimistic updates? How would you handle errors?
-      */}
-      </div>
+      {/* Table of countries */}
     </div>
   );
 }
